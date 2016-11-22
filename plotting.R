6a6e53618990b67bfaeafdc4b67ebeea1f3028b7
Katherine Scranton <katherine.scranton@ucla.edu>
2016-11-22 14:26:59 -0800

    Add files via upload

---
+++ plotting.R
@@ -0,0 +1,31 @@
+setwd("Dropbox/Office/Mentoring/Ashwin")
+
+library(readr)
+library(ggplot2)
+library(dplyr)
+library(tidyverse)
+
+
+rm(list=ls())
+
+date2month <- function(xstr){
+  months <- c("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec")
+  ans <- NA
+  for (i in 1:12){
+    if (months[i] %in% strsplit(xstr,"-")[[1]]){ans <- i}
+  }
+  ans
+}
+
+ccbug <- read.csv("ccbug_94-09_patch-density_output_FINAL.csv", stringsAsFactors = FALSE)
+
+ccbug[ ,5:11] <- sapply(ccbug[ ,5:11],as.numeric)
+ccbug$month <- sapply(ccbug$date,date2month)
+ccbug.1994 <- ccbug[ccbug$year==1994,]
+
+
+
+
+
+
+
